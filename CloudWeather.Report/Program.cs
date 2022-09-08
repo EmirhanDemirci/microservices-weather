a56edc1d39c511a374f953aa8e197819f98fdae8
Emirhan Demirci <emirhan.demirci@feironsoft.com>
2022-09-09 01:19:28 +0200

    Add data access for remaining  microservices

--- CloudWeather.Report/Program.cs
+++ CloudWeather.Report/Program.cs
@@ -1,5 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+using CloudWeather.Report.DataAccess;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WeatherReportDbContext>(
+    opts => {
+        opts.EnableSensitiveDataLogging();
+        opts.EnableDetailedErrors();
+        opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+    }, ServiceLifetime.Transient
+);
 var app = builder.Build();
 
 app.Run();
