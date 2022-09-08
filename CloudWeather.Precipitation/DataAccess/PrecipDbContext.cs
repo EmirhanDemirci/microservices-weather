ed0d8e251776c33b6a6d64be97c0d3cfe020a755
Emirhan Demirci <emirhan.demirci@feironsoft.com>
2022-09-09 01:02:59 +0200

    add data access to precipitation service

---
+++ CloudWeather.Precipitation/DataAccess/PrecipDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CloudWeather.Precipitation.DataAccess
+{
+    public class PrecipDbContext : DbContext
+    {
+        public PrecipDbContext() { }
+        public PrecipDbContext(DbContextOptions opts) : base(opts) { }
+
+        public DbSet<Precipitation> Precipitation { get; set; }
+
+        //Calling this before creating the table to apply snakecase identity
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            SnakeCaseIdentityTableNames(modelBuilder);
+        }
+        private static void SnakeCaseIdentityTableNames(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Precipitation>(b => { b.ToTable("precipitation"); });
+        }
+    }
+}
